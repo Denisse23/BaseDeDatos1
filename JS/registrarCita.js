7f0bd5e7504a4ee2025f4354b96e16df485c4862
Denisse23 <denissesuazo@hotmail.com>
2015-03-25 16:43:15 -0600

    Corrección BD, Hora en las citas, cambio tipo cita

--- JS/registrarCita.js
+++ JS/registrarCita.js
@@ -78,7 +78,27 @@ $(document).ready(function(){
 			
 		}
 	})
+	$("select[name=opcion_cita]").change(function(){
+		while(document.getElementById("tipo_mantenimiento").length>0){
+		document.getElementById("tipo_mantenimiento").remove(0);
+		}
+		while(document.getElementById("tipo_reparacion").length>0){
+		document.getElementById("tipo_reparacion").remove(0);
+		}
+		 if($("select[name=opcion_cita]").val()=='2'){
+			$('#tipo_mantenimiento').append('<option value='+'5000'+'>'+'5000'+'</option>');
+			$('#tipo_mantenimiento').append('<option value='+'10000'+'>'+'10000'+'</option>');
+			$('#tipo_mantenimiento').append('<option value='+'15000'+'>'+'15000'+'</option>');
+		 }else{
+			$('#tipo_reparacion').append('<option value='+'fallo del motor de arranque'+'>'+'fallo del motor de arranque'+'</option>');
+			$('#tipo_reparacion').append('<option value='+'fallo en el tren delantero'+'>'+'fallo en el tren delantero'+'</option>');
+			$('#tipo_reparacion').append('<option value='+'ruido cerca del motor'+'>'+'ruido cerca del motor'+'</option>'); 
+		 }
+     });
+
 });
+
+
 
 function addPlacas(data) {
 	var placas_cliente = data.split("@");
@@ -89,3 +109,87 @@ function addPlacas(data) {
 		document.getElementById("placa_cita").appendChild(new_option);
 	}
 };
+
+function RevisarHoras(){
+	
+	while(document.getElementById("hora_entrada_cita").length>0){
+		document.getElementById("hora_entrada_cita").remove(0);
+	}
+	if(document.getElementById("fecha_entrada_cita").value!="" && document.getElementById("fecha_entrada_cita").value.length==10){
+		$num =0;
+		var fechaactual = new Date();
+		var añoactual = fechaactual.getFullYear();
+		var mesactual= fechaactual.getMonth()+1;
+		var diaactual = fechaactual.getDate();
+		var horaactual = fechaactual.getHours()+1;
+		if(parseInt(mesactual)<10){
+			mesactual="0"+(fechaactual.getMonth()+1);
+		}
+		if(parseInt(diaactual)<10){
+			diaactual="0"+fechaactual.getDate();
+		}
+		var fechacampo = document.getElementById("fecha_entrada_cita").value;
+		var añocampo = parseInt(fechacampo.charAt(0)+fechacampo.charAt(1)+fechacampo.charAt(2)+fechacampo.charAt(3));
+		var mescampo;
+		var diacampo;
+		if(fechacampo.charAt(5)!='0'){
+			mescampo=parseInt(fechacampo.charAt(5)+fechacampo.charAt(6));
+		}else{
+			mescampo=parseInt(fechacampo.charAt(6));
+		}
+		if(fechacampo.charAt(8)!='0'){
+			diacampo=parseInt(fechacampo.charAt(8)+fechacampo.charAt(9));
+		}else{
+			diacampo=parseInt(fechacampo.charAt(9));
+		}
+			
+		var hayerror=false;
+		if(añocampo<parseInt(añoactual) || añocampo>2020){
+			hayerror=true;
+		}
+		if(mescampo<parseInt(mesactual) && añocampo==parseInt(añoactual)){
+			hayerror=true;
+		}
+		if(diacampo<parseInt(diaactual) && mescampo==parseInt(mesactual) && añocampo==parseInt(añoactual)){
+			hayerror=true;
+		}
+		
+		if(hayerror==false){
+			var url="./PHP/insertarCitahora.php";
+			$.ajax({type:"POST", url:url,data: $("#fr").serialize(),
+				success: function(data)
+				{
+					
+					var json=$.parseJSON(data);
+					if(json[0]!="No hay hora"){
+						
+						while(json[$num]){
+							if(json[$num].charAt(0)!=0){
+								var horavalor = parseInt(json[$num].charAt(0)+json[$num].charAt(1));
+								if(!(horavalor<parseInt(horaactual) && (fechacampo==(añoactual+"-"+mesactual+"-"+diaactual)))){
+									$('#hora_entrada_cita').append('<option value='+json[$num]+'>'+json[$num]+'</option>');
+								}
+							}else{
+								var horavalor = parseInt(json[$num].charAt(1));
+								if(!(horavalor<parseInt(horaactual) && (fechacampo==(añoactual+"-"+mesactual+"-"+diaactual)))){
+									$('#hora_entrada_cita').append('<option value='+json[$num]+'>'+json[$num]+'</option>');
+								}
+							}
+							
+							$num=$num+1;
+						}
+						
+					}else{
+						$('#hora_entrada_cita').append('<option value="no">No hay hora disponible para esta fecha</option>');
+					}
+				  
+				  
+				}
+			});
+			
+		}
+		
+		
+	}
+	
+}//fin de la funcion
